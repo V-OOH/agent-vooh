b7083e2c6eb62c2b4953c90015028e1103e42ee9
MarleyS439 <marleysantos439@gmail.com>
2026-03-28 14:18:22 -0300

    Função de detecção de informações em nível de software

---
+++ src/detectar/software.py
@@ -0,0 +1,43 @@
+import platform, getpass
+
+
+# noinspection SpellCheckingInspection
+def software() -> dict[str, str]:
+    """
+    Função para detectar as informações de software do dispositivo
+
+    Obtém as informações de:
+        - Sistema (Linux ou Windows)
+        - Kernel
+        - Versão do Kernel
+        - Host (Nome de máquina)
+        - Usuário
+
+    Returns: Lista de informações do software
+    """
+
+    # Informações do sistema (Linux ou Windows)
+    sistema = platform.system()
+
+    # Informações do Kernel
+    kernel = platform.release()
+
+    # Versão do Kernel
+    versao = platform.version()
+
+    # Host
+    host = platform.node()
+
+    # Usuário
+    usuario = getpass.getuser()
+
+    # Dicionário de informações
+    informacoes = {
+        "sistema" : sistema,
+        "kernel": kernel,
+        "versao": versao,
+        "host": host,
+        "usuario": usuario
+    }
+
+    return informacoes
