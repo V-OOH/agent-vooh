94ecb954f4e0e9752ed72ddec24e41fba2a0591f
MarleyS439 <marleysantos439@gmail.com>
2026-04-03 14:13:46 -0300

    Função de captura de dados do disco

--- src/monitor/hardware/disco.py
+++ src/monitor/hardware/disco.py
@@ -1,31 +1,51 @@
-import psutil
+import sys, psutil
 
-def info_disco(plataforma: str) -> dict:
+def info_disco(plataforma: str) -> list[dict[str, str]]:
     """
-    Função para ler dtalhadamente os dados do disco.
+    Faz a captura de dados do disco
 
     Args:
         plataforma (str): Windows ou Linux
 
-    Returns: Dicionário de informações do disco
+    Returns:
+        Dicionário de informações do disco:
+
+            - Total do disco em bytes
+
+            - Total usado do disco em bytes
+
+            - Total livre do disco em bytes
+
+            - Percentual de uso do disco
     """
 
+    # Root do sistema
     root = ""
+
+    # Lista de discos
+    lista_disco = []
 
     if plataforma == "Linux":
         root = "/"
     elif plataforma == "Windows":
         root = "C:\\"
+    else:
+        print("Plataforma não suportada: " + plataforma)
+        sys.exit(1)
 
-    # Objeto de disco do psutil
+    # Objeto de disco
     disco = psutil.disk_usage(root)
 
-    #
-
-    # Informações do disco
-    info = {
-
+    # Dados
+    dados = {
+        "total": disco.total,
+        "usado": disco.used,
+        "livre": disco.free,
+        "percentual": disco.percent
     }
 
+    # Adiciona na lista de informações do disco os dados
+    lista_disco.append(dados)
 
-    return info
+    # Retorna a lista
+    return lista_disco
