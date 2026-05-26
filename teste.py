bab12a8e621f4d9e5409d55a368a74d57414bca8
MarleyS439 <marleysantos439@gmail.com>
2026-05-25 21:00:29 -0300

    Remoção de arquivo de teste desnecessário

--- teste.py
+++
@@ -1,8 +0,0 @@
-from detectar import identificar
-from src.database.database import buscar_id_display
-
-busca = buscar_id_display("a4:63:a1:6e:67:09")
-print(busca)
-
-i = identificar("Linux")
-print(i)
